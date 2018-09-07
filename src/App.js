e3ab8a7d79f8de0b14043d9dd8e2855adfe4a45a
andreaweb <deiainluv@gmail.com>
2018-09-07 11:03:09 -0300

    separated components into folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Header from './Header/Header.js';
+import SelectedProduct from './SelectedProduct/SelectedProduct.js';
+import Modal from './Modal/Modal.js';
 import './App.css';
 
 class App extends Component {
@@ -62,100 +65,19 @@ class App extends Component {
   render() {
     return (
       <div className={ this.state.isModalOpen ? "App App--modal-open" : "App" }>
-        <header className="header">
-         <img src="./images/logo.png" className="logo" alt="logo" />
-        </header>
+        <Header />
 
-        <div className="selected-product">
-          <img src={this.state.product.image} className="product-image" alt="Product" />
+        <SelectedProduct 
+          product={this.state.product} 
+          checkout={this.state.checkout} 
+          selectedCoupon={this.state.selectedCoupon}
+          selectedDiscount={this.state.selectedDiscount}
+          couponSelect={(id, discount) => this.couponSelect(id, discount)}
+          cancelPurchase={this.cancelPurchase}
+          sendPurchase={this.sendPurchase}
+        />
 
-          <div className="checkout">
-            <div className="coupons">
-              <h5 className="checkout__title">cupons</h5>
-
-              <div className="coupon__item">
-                <input 
-                  id="none" 
-                  name="cupom" 
-                  className="radio"
-                  onChange={() => this.couponSelect("none")} 
-                  checked={this.state.selectedCoupon === "none"} 
-                  type="radio"
-                />
-                <label className="coupon__label" htmlFor="none">
-                  <span className="coupon__title">não usar cupom</span>
-                </label>
-              </div>
-
-              { this.state.checkout.availableCoupons 
-                ? this.state.checkout.availableCoupons.map((coupon) =>
-                  <div className="coupon__item" key={coupon.id}>
-                    <input 
-                      type="radio"
-                      className="radio" 
-                      onChange={() => this.couponSelect(coupon.id, coupon.discount)} 
-                      checked={this.state.selectedCoupon === coupon.id} 
-                      name="cupom" 
-                      id={coupon.id} 
-                    />
-                    <label htmlFor={coupon.id} className="coupon__label">
-                      <span className="coupon__title">
-                        {coupon.title} 
-                      </span>
-                      <span className="coupon__discount">- R$ {coupon.discount},00</span>
-                    </label>
-                  </div>
-                  )
-                : null
-              }
-              
-            </div>
-            <div className="summary">
-              <h5 className="checkout__title">resumo</h5>
-              <p className="summary__item">
-                <span>valor original</span>
-                <span>R$ {this.state.product.price},00</span>
-              </p>
-              <p className="summary__item">
-                <span>cupom</span>
-                <span className="red">-R$ {this.state.selectedDiscount},00</span>
-              </p>
-              <p className="summary__item">
-                <span>frete</span>
-                <span>R$ {this.state.checkout.shippingPrice},00</span>
-              </p>
-              <p className="summary__item">
-                <span>total</span>
-                <span className="bold">R$ {  this.state.checkout.totalPrice},00</span>
-              </p>
-            </div>
-            <div className="buttons">
-              <button className="button button--cancel" onClick={this.cancelPurchase}>cancelar</button>
-              <button  className="button button--confirm" onClick={this.sendPurchase}>confirmar</button>
-            </div>
-          </div>
-        </div>
-
-        <div 
-          className={ this.state.isModalOpen ? "modal" : "display-none"} 
-          modaltype={ this.state.modalType }
-        >
-          <section className="modal__content">
-            <img 
-              src={ this.state.modalType == "confirm" 
-                    ? "./images/cart.png" 
-                    : "./images/orange-cart.png"
-                  }
-              className="modal__image" 
-            />
-            <h4 className="modal__title">
-              compra confirmada
-            </h4>
-            <p className="modal__description">
-              enviaremos atualizações sobre o pedido para o seu email
-            </p>
-          </section>
-        </div>
+        <Modal open={this.state.isModalOpen} type={this.state.modalType} />
       </div>
     );
   }
---
+++ src/Modal/Modal.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Modal extends Component{
+	render(){
+		return (
+			<div 
+	          className={ this.props.open ? "modal" : "display-none"} 
+	          modaltype={ this.props.type }
+	        >
+	          <section className="modal__content">
+	            <img 
+	              src={ this.props.type === "confirm" 
+	                    ? "./images/cart.png" 
+	                    : "./images/orange-cart.png"
+	                  }
+	              className="modal__image" 
+	            />
+	            <h4 className="modal__title">
+	              compra 
+	              	{ 
+	              		this.props.type === "confirm" 
+	                    ? " confirmada" 
+	                    : null
+	                }
+	                { 
+	              		this.props.type === "cancel" 
+	                    ? " cancelada" 
+	                    : null
+	                }
+	                { 
+	              		this.props.type === "error" 
+	                    ? " não concluída" 
+	                    : null
+	                }
+	            </h4>
+	            <p className="modal__description">
+	            	{ 
+	              		this.props.type === "confirm" 
+	                    ? "enviaremos atualizações sobre o pedido para o seu email" 
+	                    : null
+	                }
+	                { 
+	              		this.props.type === "cancel" 
+	                    ? "o pedido não foi enviado e você não será cobrado" 
+	                    : null
+	                }
+	                { 
+	              		this.props.type === "error" 
+	                    ? "um erro ocorreu. confira sua conexão de internet ou tente mais tarde" 
+	                    : null
+	                } 
+	            </p>
+	          </section>
+	        </div>
+		)
+	}
+}
+
+export default Modal
